5e369ae86e9a28497cb0f50efcfeb17bedc7e245
rahmaa4 <rahmagudal@gmail.com>
2025-05-26 20:26:13 +0100

    fix scroll

--- src/components/TrendsChart/Chart/Chart.tsx
+++ src/components/TrendsChart/Chart/Chart.tsx
@@ -4,7 +4,7 @@ import Bar from "./Bar/Bar"
 export default function Chart({ userEntries }: { userEntries: MoodEntries }) {
    
     return (
-        <div className={`flex gap-4 overflow-x-scroll lg:overflow-visible`}>
+        <div className={`flex gap-4 overflow-x-auto`}>
             {
                 [...userEntries].reverse().map((entry, index) => {
                     return <Bar entryData={entry} key={`bar-${index}`} position={index} /> 
